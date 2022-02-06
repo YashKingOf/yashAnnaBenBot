0edcc89acb521b996c3ca0d6eafd233df5aa2423
ALLUKISHORE <98156472+ALLUKISHORE@users.noreply.github.com>
2022-02-06 16:21:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -401,8 +401,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url='http://t.me/AKAutoFilterBot?startgroup=true')
             ],[
             InlineKeyboardButton('sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('ʀᴇqᴜᴇꜱᴛ',url='https://t.me/ALLU_KISHORE_MOVIE_REQUEST')
-            InlineKeyboardButton('ʀᴇqᴜᴇꜱᴛ >> ₂',url='https://t.me/ALLU_KISHORE_MOVIE_REQUEST')
+            InlineKeyboardButton('ʀᴇqᴜᴇꜱᴛ',url='https://t.me/ALLU_KISHORE_MOVIE_REQUEST'),
+            InlineKeyboardButton('ʀᴇqᴜᴇꜱᴛ >> ₂',url='https://t.me/+5PTmhXVDCHNiMDk9')
             ],[
             InlineKeyboardButton('yᴏᴜᴛᴜʙᴇ ', url='https://youtube.com/channel/UCjKZr0-f0SelNT-4XBO13RQ'),
             InlineKeyboardButton('ɪɴꜱᴛᴀɢʀᴀᴍ ', url='https://instagram.com/allukishore.official?utm_medium=copy_link'),
