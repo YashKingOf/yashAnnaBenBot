bec7810afdde738ce4de574afd2b3600b2586b9e
ALLUKISHORE <98156472+ALLUKISHORE@users.noreply.github.com>
2022-02-07 19:49:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,7 +37,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url='http://t.me/AKAutoFilterBot?startgroup=true')
+            InlineKeyboardButton('ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url='http://t.me/AKAutoFilterBot?startgroup=true'),
             InlineKeyboardButton('sᴇᴀʀᴄʜ', switch_inline_query_current_chat='')
             ],[
             InlineKeyboardButton('ᴜᴩᴅᴀᴛᴇꜱ',url='https://t.me/+nfsmDRsL3j5lZjU1'),
